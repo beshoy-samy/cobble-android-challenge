afba089c974c7ba5b29bd4adad6631695c1a62e3
Beshoy Samy <beshoy.samy411@gmail.com>
2021-10-15 15:43:02 +0200

    migrated to kts with buildSrc

---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    id(Build.Plugins.androidApplicationPlugin)
+    kotlin(Build.Plugins.kotlinAndroidPlugin)
+    kotlin(Build.Plugins.kotlinKaptPlugin)
+    id(Build.Plugins.hiltAndroidPlugin)
+}
+
+android {
+
+    compileSdk = Config.AndroidProject.compileSdkVersion
+
+    defaultConfig {
+        applicationId = "com.cobble.challenge"
+        minSdk = Config.AndroidProject.minSdkVersion
+        targetSdk = Config.AndroidProject.targetSdkVersion
+        versionCode = Config.AndroidProject.versionCode
+        versionName = Config.AndroidProject.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+
+        release {
+            isMinifyEnabled = Config.isReleaseMinifyEnabled
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+    }
+
+    compileOptions {
+        sourceCompatibility = Config.javaVersion
+        targetCompatibility = Config.javaVersion
+    }
+
+    kotlinOptions {
+        jvmTarget = Config.javaVersion.toString()
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+}
+
+dependencies {
+    withAndroidX()
+    withHilt()
+    withNavigationComponent()
+    withTestDependencies()
+}
+
+kapt {
+    correctErrorTypes = true
+}
---
+++ buildSrc/src/main/kotlin/AppModules.kt
@@ -0,0 +1,4 @@
+import org.gradle.kotlin.dsl.DependencyHandlerScope
+import org.gradle.kotlin.dsl.project
+
+object AppModules
